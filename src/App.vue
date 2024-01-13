d28162d6b3a3698a4dc17345d56df8387abf7313
ErtelDG <ertel.developer@t-online.de>
2024-01-13 18:40:59 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <fwb-spinner color="blue" />
+  <fwb-spinner color="gray" />
+  <fwb-spinner color="green" />
+  <fwb-spinner color="pink" />
+  <fwb-spinner color="purple" />
+  <fwb-spinner color="red" />
+  <fwb-spinner color="white" />
+  <fwb-spinner color="yellow" />
+</template>
+
+<script setup>
+import { FwbSpinner } from 'flowbite-vue'
+</script>
