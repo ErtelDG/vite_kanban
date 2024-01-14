36b59883308456862c21c07fe46578fcb861d134
ErtelDG <ertel.developer@t-online.de>
2024-01-14 11:22:54 +0100

    general_design

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,57 @@
 <template>
-  <fwb-spinner color="blue" />
-  <fwb-spinner color="gray" />
-  <fwb-spinner color="green" />
-  <fwb-spinner color="pink" />
-  <fwb-spinner color="purple" />
-  <fwb-spinner color="red" />
-  <fwb-spinner color="white" />
-  <fwb-spinner color="yellow" />
+   <div class="max-w-full h-screen">
+      <Navbar></Navbar>
+      <Sidebar></Sidebar>
+      <KanbanMain></KanbanMain>
+   </div>
 </template>
 
 <script setup>
-import { FwbSpinner } from 'flowbite-vue'
+import { FwbCard } from "flowbite-vue";
+import Navbar from "./components/Navbar.vue";
+import Sidebar from "./components/Sidebar.vue";
+import KanbanMain from "./components/KanbanMain.vue";
+
+import { onMounted } from "vue";
+
+onMounted(() => {
+   var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+   var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+
+   // Change the icons inside the button based on previous settings
+   if (localStorage.getItem("color-theme") === "dark" || (!("color-theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+      themeToggleLightIcon.classList.remove("hidden");
+   } else {
+      themeToggleDarkIcon.classList.remove("hidden");
+   }
+
+   var themeToggleBtn = document.getElementById("theme-toggle");
+
+   themeToggleBtn.addEventListener("click", function () {
+      // toggle icons inside button
+      themeToggleDarkIcon.classList.toggle("hidden");
+      themeToggleLightIcon.classList.toggle("hidden");
+
+      // if set via local storage previously
+      if (localStorage.getItem("color-theme")) {
+         if (localStorage.getItem("color-theme") === "light") {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("color-theme", "dark");
+         } else {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("color-theme", "light");
+         }
+
+         // if NOT set via local storage previously
+      } else {
+         if (document.documentElement.classList.contains("dark")) {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("color-theme", "light");
+         } else {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("color-theme", "dark");
+         }
+      }
+   });
+});
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,11 @@ export default {
       "./src/**/*.{vue,js,ts,jsx,tsx}",
       "node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}",
       "node_modules/flowbite/**/*.{js,jsx,ts,tsx}",
+      "./node_modules/flowbite/**/*.js",
    ],
    theme: {
       extend: {},
    },
    plugins: [require("flowbite/plugin")],
+   darkMode: "class",
 };
