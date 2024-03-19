22bb5c32a8cb9053422978ffc43db72aea7f3fbb
ErtelDG <ertel.developer@t-online.de>
2024-03-19 20:19:24 +0100

    Commit: 1710875964

--- src/components/SlideCurrentTask.vue
+++ src/components/SlideCurrentTask.vue
@@ -356,6 +356,7 @@ const removeCurrentTask = async (id) => {
 
    const requestOptions = {
       method: "DELETE",
+      mode: "no-cors",
       headers: { "Content-Type": "application/json" },
       body: delTask,
    };
